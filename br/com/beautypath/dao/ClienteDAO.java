2f7c2f9bed8db5ad2fabeb7053f16484ea2eaaac
yuribreion1 <yuribreion@gmail.com>
2018-09-17 00:28:36 -0300

    adicionando sequencia a chave primaria

--- br/com/beautypath/dao/ClienteDAO.java
+++ br/com/beautypath/dao/ClienteDAO.java
@@ -21,13 +21,12 @@ public class ClienteDAO {
 	 */
 	
 	public String gravar(Cliente cli, Connection conexao) throws SQLException {
-		String sql = "insert into rm79935.cliente(COD_CLI, NOME_CLI, TEL_CLI, EMAIL_CLI, SOCIAL_URL_CLI) values (?, ?, ?, ?, ?)";
+		String sql = "insert into rm79935.cliente(COD_CLI, NOME_CLI, TEL_CLI, EMAIL_CLI, SOCIAL_URL_CLI) values (SEQ_CLIENTE.NEXTVAL, ?, ?, ?, ?)";
 		ps = conexao.prepareStatement(sql);
-		ps.setInt(1, cli.getIdCliente());
-		ps.setString(2, cli.getNome());
-		ps.setString(3, cli.getTelefone());
-		ps.setString(4, cli.getEmail());
-		ps.setString(5, cli.getSocialUrl());
+		ps.setString(1, cli.getNome());
+		ps.setString(2, cli.getTelefone());
+		ps.setString(3, cli.getEmail());
+		ps.setString(4, cli.getSocialUrl());
 		ps.execute();
 		ps.close();
 		return "Cliente gravado com sucesso";
@@ -36,7 +35,7 @@ public class ClienteDAO {
 	/**
 	 * 
 	 * @param numero
-	 * @return execução do comando delete na tabela
+	 * @return execucao do comando delete na tabela
 	 * @throws SQLException
 	 * @author Yuri Breion
 	 */
