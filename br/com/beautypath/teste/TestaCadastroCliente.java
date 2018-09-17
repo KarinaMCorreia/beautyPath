8ffc8650d2e7a55ac8390b02ae25e25ce39653d2
yuribreion1 <yuribreion@gmail.com>
2018-09-17 00:30:11 -0300

    Aplicando correcao para uso da DAO

--- br/com/beautypath/teste/TestaCadastroCliente.java
+++ br/com/beautypath/teste/TestaCadastroCliente.java
@@ -1,6 +1,11 @@
 package br.com.beautypath.teste;
 
+import java.sql.Connection;
+
+import javax.swing.JOptionPane;
+
 import br.com.beautypath.dao.ClienteDAO;
+import br.com.beautypath.dao.ConnectionFactory;
 import br.com.beautypath.modelo.Cliente;
 
 public class TestaCadastroCliente {
@@ -8,10 +13,18 @@ public class TestaCadastroCliente {
 	public static void main(String[] args) {
 		
 		try {
+			String user = JOptionPane.showInputDialog("Digite seu usuário:");
+			String pw = JOptionPane.showInputDialog("Digite a sua senha:");
+			Connection c = ConnectionFactory.controlarInstancia().getConnection(user, pw);
+			
 			Cliente cli = new Cliente();
 			ClienteDAO dao = new ClienteDAO();
-			cli.setAll(1, "Yuri Breion", "998065815", "yuribreion@br.ibm.com", "@yuribreion");
-			dao.gravar(cli, null);
+			
+			cli.setNome(JOptionPane.showInputDialog("Digite o nome do cliente:"));
+			cli.setTelefone(JOptionPane.showInputDialog("Digite o telefone do cliente:"));
+			cli.setSocialUrl(JOptionPane.showInputDialog("Entre com a rede social do cliente:"));
+			cli.setEmail(JOptionPane.showInputDialog("Digite o e-mail do cliente:"));
+			dao.gravar(cli, c);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
