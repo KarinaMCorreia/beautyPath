5c81becbdc3177e22af15ff863abffbb86766bd1
yuribreion1 <yuribreion@gmail.com>
2018-09-17 00:29:10 -0300

    Criando construtor para tratar as excecoes

--- br/com/beautypath/excecao/Excecao.java
+++ br/com/beautypath/excecao/Excecao.java
@@ -1,7 +1,12 @@
 package br.com.beautypath.excecao;
 
-public class Excecao {
+public class Excecao extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Excecao(Exception e) {
 		if (e.getClass().getName().equals("java.lang.NumberFormatException")) {
 			System.out.println("Erro de preenchimento\n");
@@ -9,14 +14,18 @@ public class Excecao {
 			System.out.println("Erro na sintaxe do script sql");
 		}
 	}
-	
+
+	public Excecao(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public static String getErro(Exception e) {
 		if (e.getClass().getName().equals("java.lang.NumberFormatException")) {
 			return "Erro de preenchimento\n";
 		} else if (e.getClass().getName().equals("java.sql.SQLSyntaxErrorException")) {
 			System.out.println("Erro na sintaxe do script sql");
 		} else {
-			return "Erro ainda não mapeado";
+			return "Erro ainda nao mapeado";
 		}
 		return "Houve um erro";
 	}
